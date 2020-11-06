21d0a7aef18da7a1944d1484d6cb06e5b2fd83af
Eric Xu <avatar.xu@gmail.com>
2020-11-06 16:35:45 +0800

    v 3 Update np.py

--- common/np.py
+++ common/np.py
@@ -1,11 +1,16 @@
 # coding: utf-8
 from common.config import GPU
+from common.config import Device as GPU_Device
 
 
 if GPU:
     import cupy as np
-    np.cuda.set_allocator(np.cuda.MemoryPool().malloc)
-    np.add.at = np.scatter_add
+
+    np.cuda.Device(GPU_Device).use()
+    print('Use GPU: %d' % (GPU_Device,))
+
+    pool = np.cuda.MemoryPool(np.cuda.malloc_managed)
+    np.cuda.set_allocator(pool.malloc)
 
     print('\033[92m' + '-' * 60 + '\033[0m')
     print(' ' * 23 + '\033[92mGPU Mode (cupy)\033[0m')
